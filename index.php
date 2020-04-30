841394549f20e9f7f7e32afcf9164e899133ac44
Swaraj Dhondge <33777798+kaaaaate009@users.noreply.github.com>
2020-04-30 12:56:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,35 @@
 <?php 
-header("Location: with css/index.html");
-die(); 
+// header("Location: with css/index.html");
+// die(); 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>	
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Premeier League Football</title>
+  <style>
+  ul
+  {
+    list-style-type: none;
+  }
+  li
+  {
+    font-size: 15px;
+  }
+  li:before
+  {
+    content:"@  ";
+    color: red;
+    font-size: 25px;
+  }
+</style>
+</head>
+<body>
+  <ul>
+    <li><span><a href="with css/index.html">With CSS</a><span></li>
+    <li><span><a href="without css/index.html">Without CSS</a><span></li>
+  </ul>
+</body>
+</html>
